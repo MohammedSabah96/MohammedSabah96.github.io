7abeaa1b45208bee595e0309e9d9e890adf138a1
Mohammed <mohammedsabah455@gmail.com>
2019-03-23 17:37:38 +0300

    RobotFriends

---
+++ build/precache-manifest.404183839254c7da7409c4028dc2636c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "00be7ea27308308b1645",
+    "url": "/static/js/main.5729656c.chunk.js"
+  },
+  {
+    "revision": "bc4e2a328c6e48408476",
+    "url": "/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "00be7ea27308308b1645",
+    "url": "/static/css/main.1c2db9ba.chunk.css"
+  },
+  {
+    "revision": "bc4e2a328c6e48408476",
+    "url": "/static/css/2.7df07b7a.chunk.css"
+  },
+  {
+    "revision": "718ddcc22b643527c968735889aba171",
+    "url": "/index.html"
+  }
+];
